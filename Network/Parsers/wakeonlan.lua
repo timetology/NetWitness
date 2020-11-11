97a2bb4b145e9eb0e0d25815e9c5aa0fb059c9df
timetology <timetology@gmail.com>
2020-11-11 13:57:19 -0500

    Create wakeonlan.lua

---
+++ Network/Parsers/wakeonlan.lua
@@ -0,0 +1,21 @@
+--Identified the Magic Packet for Wake on Lan
+--https://en.wikipedia.org/wiki/Wake-on-LAN#Magic_packet
+
+-- Step 1 - Create parser
+local wakeonlan = nw.createParser("WakeOnLan-parser", "Wake On Lan Parser")
+
+-- Step 2 - Define meta keys to write meta into
+wakeonlan:setKeys({
+	nwlanguagekey.create("analysis.service", nwtypes.Text),
+})
+
+-- Step 4 - Do SOMETHING once your token matched
+function wakeonlan:IOC(token, first, last)
+	nw.createMeta(self.keys["analysis.service"], "wake on lan") 
+end
+
+-- Step 3 - Define tokens that get you close to what you want
+wakeonlan:setCallbacks({
+	["\255\255\255\255\255\255"] = wakeonlan.IOC, -- "FF FF FF FF FF FF" in Hex
+
+})
